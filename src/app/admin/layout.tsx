b18664df6d527ce925d04b5fd1780fb4971621f7
Axel Aranda <axelemilianoaranda1993@gmail.com>
2026-05-12 12:26:30 -0300

    protección de ruta admin y layout de admin

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,14 @@
+import ProtectedRoute from "@/components/auth/ProtectedRoute";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ProtectedRoute>
+      {children}
+    </ProtectedRoute>
+  );
+}
+
