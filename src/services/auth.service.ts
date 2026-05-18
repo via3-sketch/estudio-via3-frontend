ad9b868c283744c918b61690091574a8b814a0f6
TFColmenares <131204344+TFColmenares@users.noreply.github.com>
2026-05-18 10:32:39 -0500

    Refactor completeProfile to use passed token
    Refactor completeProfile function to accept token as parameter and remove redundant token retrieval.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,46 +1,35 @@
+// auth.service.ts
 import { api } from "./api";
 
 export const registerUser = (payload: any) => {
   return api("/auth/signup", {
     method: "POST",
-
     body: JSON.stringify(payload),
   });
 };
 
 export const loginUser = (payload: {
   email: string;
-
   password: string;
 }) => {
   return api("/auth/signin", {
     method: "POST",
-
     body: JSON.stringify(payload),
   });
 };
 
 export const completeProfile = async (
   id: string,
+  token: string,
   payload: {
     phone: string;
     country: string;
     companyName: string;
     city: string;
     address: string;
-  },
-) => {
-    const token = document.cookie
-    .split("; ")
-    .find((row) =>
-      row.startsWith("userSession="),
-    )
-    ?.split("=")[1];
-    console.log(token);
-
-  if (!token) {
-    throw new Error("No token found");
   }
+) => {
+  if (!token) throw new Error("No token found");
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/users/complete-profile/${id}`,
@@ -61,4 +50,3 @@ export const completeProfile = async (
 
   return res.json();
 };
-
